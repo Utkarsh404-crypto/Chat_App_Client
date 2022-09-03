4eb2347752cc0e5eb73ca6a2f00e8059cf864838
Utkarsh404-crypto <uksingh2872000@gmail.com>
2022-09-03 22:48:51 +0530

    reset

--- src/Context/ChatContext.js
+++ src/Context/ChatContext.js
@@ -4,41 +4,40 @@ import { useHistory } from "react-router-dom";
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-	const [ref, setRef] = useState(false);
-	const [user, setUser] = useState();
-	const [slectedchat, setSelectedChat] = useState();
-	const [chats, setChats] = useState([]);
-	const [notifications, setNotifications] = useState([]);
-	const history = useHistory();
+    const [ref, setRef] = useState(false);
+    const [user, setUser] = useState();
+    const [slectedchat, setSelectedChat] = useState();
+    const [chats, setChats] = useState([]);
+    const [notifications, setNotifications] = useState([]);
+    const history = useHistory();
 
-	useEffect(() => {
-		const userInfo = JSON.parse(localStorage.getItem("userInfo"));
-		setUser(userInfo);
-		if (!userInfo) history.push("/");
-	}, [history]);
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+        setUser(userInfo);
+        if (!userInfo) history.push("/");
+    }, [history]);
 
-	return (
-		<ChatContext.Provider
-			value={{
-				user,
-				setUser,
-				slectedchat,
-				setSelectedChat,
-				chats,
-				setChats,
-				ref,
-				setRef,
-				notifications,
-				setNotifications
-			}}>
-			{" "}
-			{children}{" "}
-		</ChatContext.Provider>
-	);
+    return ( <
+        ChatContext.Provider value = {
+            {
+                user,
+                setUser,
+                slectedchat,
+                setSelectedChat,
+                chats,
+                setChats,
+                ref,
+                setRef,
+                notifications,
+                setNotifications
+            }
+        } > { " " } { children } { " " } <
+        /ChatContext.Provider>
+    );
 };
 
 export const ChatState = () => {
-	return useContext(ChatContext);
+    return useContext(ChatContext);
 };
 
 export default ChatProvider;
